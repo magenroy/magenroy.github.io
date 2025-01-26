9da6d6fa3296754b2756b6447911b78ed138b119
Roy Magen <magenroy@gmail.com>
2025-01-26 13:30:54 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use leptos::*;
+use prelude::*;
 use mount::mount_to_body;
 
 #[component]
 fn App() -> impl IntoView {
     view! {
     <div class="container">
-
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
 
         <h1>"Welcome to Leptos"</h1>
         <h2><i>"On Github Pages"</i></h2>
