50bff7cba4eda186a596fea0971fd0ff74f45492
Roy Magen <magenroy@gmail.com>
2025-01-28 17:55:31 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,18 @@ const FAVICON: Asset = asset!("/assets/graphics/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 
 fn main() {
-    // dioxus::launch(App);
+    dioxus::launch(App);
 
     // REF: https://dioxuslabs.com/blog/release-060#static-site-generation-and-isg
-    dioxus::LaunchBuilder::new()
-        .with_cfg(server_only! {
-            ServeConfig::builder()
-                // turn on incremental site generation with the .incremental() method
-                .incremental(IncrementalRendererConfig::new())
-                .build()
-                .unwrap()
-        })
-        .launch(App)
+    // dioxus::LaunchBuilder::new()
+    //     .with_cfg(server_only! {
+    //         ServeConfig::builder()
+    //             // turn on incremental site generation with the .incremental() method
+    //             .incremental(IncrementalRendererConfig::new())
+    //             .build()
+    //             .unwrap()
+    //     })
+    //     .launch(App)
 }
 
 #[component]
@@ -54,10 +54,10 @@ fn App() -> Element {
     }
 }
 // REF: https://dioxuslabs.com/blog/release-060/#static-site-generation-and-isg
-#[server(endpoint = "static_routes")]
-async fn static_routes() -> Result<Vec<String>, ServerFnError> {
-    Ok(Route::static_routes()
-        .into_iter()
-        .map(|route| route.to_string())
-        .collect::<Vec<_>>())
-}
+// #[server(endpoint = "static_routes")]
+// async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+//     Ok(Route::static_routes()
+//         .into_iter()
+//         .map(|route| route.to_string())
+//         .collect::<Vec<_>>())
+// }
