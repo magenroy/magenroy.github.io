2d56b986eede3ed730d02cd692f7fc9b6bb6ebd6
Roy Magen <magenroy@gmail.com>
2025-01-28 17:03:33 -0500

    try uploading "docs" instead of "docs/public" to see if server works?

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,18 @@ const FAVICON: Asset = asset!("/assets/graphics/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 
 fn main() {
-    dioxus::launch(App);
+    // dioxus::launch(App);
+
+    // REF: https://dioxuslabs.com/blog/release-060#static-site-generation-and-isg
+    dioxus::LaunchBuilder::new()
+        .with_cfg(server_only! {
+            ServeConfig::builder()
+                // turn on incremental site generation with the .incremental() method
+                .incremental(IncrementalRendererConfig::new())
+                .build()
+                .unwrap()
+        })
+        .launch(App)
 }
 
 #[component]
@@ -42,6 +53,7 @@ fn App() -> Element {
         Router::<Route> {}
     }
 }
+// REF: https://dioxuslabs.com/blog/release-060/#static-site-generation-and-isg
 #[server(endpoint = "static_routes")]
 async fn static_routes() -> Result<Vec<String>, ServerFnError> {
     Ok(Route::static_routes()
@@ -49,5 +61,3 @@ async fn static_routes() -> Result<Vec<String>, ServerFnError> {
         .map(|route| route.to_string())
         .collect::<Vec<_>>())
 }
-
-// TODO: https://dioxuslabs.com/blog/release-060/#static-site-generation-and-isg
