9bee3c6d14a38b55e21bf48ceaea1bf757c235ec
Roy Magen <magenroy@gmail.com>
2025-01-28 18:17:23 -0500

    try copying the static files to docs/public?

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,18 @@ const FAVICON: Asset = asset!("/assets/graphics/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 
 fn main() {
-    dioxus::launch(App);
+    // dioxus::launch(App);
 
     // REF: https://dioxuslabs.com/blog/release-060#static-site-generation-and-isg
-    // dioxus::LaunchBuilder::new()
-    //     .with_cfg(server_only! {
-    //         ServeConfig::builder()
-    //             // turn on incremental site generation with the .incremental() method
-    //             .incremental(IncrementalRendererConfig::new())
-    //             .build()
-    //             .unwrap()
-    //     })
-    //     .launch(App)
+    dioxus::LaunchBuilder::new()
+        .with_cfg(server_only! {
+            ServeConfig::builder()
+                // turn on incremental site generation with the .incremental() method
+                .incremental(IncrementalRendererConfig::new())
+                .build()
+                .unwrap()
+        })
+        .launch(App)
 }
 
 #[component]
@@ -39,11 +39,10 @@ fn App() -> Element {
     // Build cool things ✌️
 
     rsx! {
-        // Meta stuff not working?
         document::Meta { name: "description", content: "Website of Roy Magen" }
         document::Meta { name: "author", content: "Roy Magen" }
-        document::Meta { property: "description", content: "Website of Roy Magen" }
-        document::Meta { property: "name", content: "Roy Magen" }
+        // document::Meta { property: "description", content: "Website of Roy Magen" }
+        // document::Meta { property: "name", content: "Roy Magen" }
 
         // Global app resources
         document::Link { rel: "icon", href: FAVICON }
@@ -54,10 +53,10 @@ fn App() -> Element {
     }
 }
 // REF: https://dioxuslabs.com/blog/release-060/#static-site-generation-and-isg
-// #[server(endpoint = "static_routes")]
-// async fn static_routes() -> Result<Vec<String>, ServerFnError> {
-//     Ok(Route::static_routes()
-//         .into_iter()
-//         .map(|route| route.to_string())
-//         .collect::<Vec<_>>())
-// }
+#[server(endpoint = "static_routes")]
+async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+    Ok(Route::static_routes()
+        .into_iter()
+        .map(|route| route.to_string())
+        .collect::<Vec<_>>())
+}
