804216a5a243f7513a5a039019e2f38cef8ffc4a
Roy Magen <magenroy@gmail.com>
2026-03-22 12:12:19 +0200

    update to Rust 2024 and Dioxus 0.7.3

--- src/main.rs
+++ src/main.rs
@@ -50,16 +50,29 @@ enum Route {
 fn main() {
     // dioxus::launch(App);
 
-    // REF: https://dioxuslabs.com/blog/release-060#static-site-generation-and-isg
+    // SOURCE: https://dioxuslabs.com/learn/0.7/essentials/fullstack/static_site_generation#setting-up-the-serveconfig
     dioxus::LaunchBuilder::new()
+        // Set the server config only if we are building the server target
         .with_cfg(server_only! {
             ServeConfig::builder()
-                // turn on incremental site generation with the .incremental() method
-                .incremental(IncrementalRendererConfig::new())
-                .build()
-                .unwrap()
+                // Enable incremental rendering
+                .incremental(
+                    dioxus::server::IncrementalRendererConfig::new()
+                        // Store static files in the public directory where other static assets like wasm are stored
+                        .static_dir(
+                            std::env::current_exe()
+                                .unwrap()
+                                .parent()
+                                .unwrap()
+                                .join("public")
+                        )
+                        // Don't clear the public folder on every build. The public folder has other files including the wasm
+                        // binary and static assets required for the app to run
+                        .clear_cache(false)
+                )
+                .enable_out_of_order_streaming()
         })
-        .launch(App)
+        .launch(App);
 }
 
 #[component]
@@ -94,11 +107,16 @@ fn PageNotFound(route: Vec<String>) -> Element {
     }
 }
 
-// REF: https://dioxuslabs.com/blog/release-060/#static-site-generation-and-isg
-#[server(endpoint = "static_routes")]
+// SOURCE: https://dioxuslabs.com/learn/0.7/essentials/fullstack/static_site_generation#configuring-static-routes
+//
+// The server function at the endpoint "static_routes" will be called by the CLI to generate the list of static
+// routes. You must explicitly set the endpoint to `"static_routes"` in the server function attribute instead of
+// the default randomly generated endpoint.
+#[server(endpoint = "static_routes", output = server_fn::codec::Json)]
 async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+    // The `Routable` trait has a `static_routes` method that returns all static routes in the enum
     Ok(Route::static_routes()
-        .into_iter()
-        .map(|route| route.to_string())
-        .collect::<Vec<_>>())
+        .iter()
+        .map(ToString::to_string)
+        .collect())
 }
