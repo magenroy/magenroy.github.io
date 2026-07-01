0f58ff38dd58c3b31830ea14d70c6643f0f20a15
Roy Magen <magenroy@gmail.com>
2026-07-01 10:26:28 +0300

    move to components

---
+++ src/components/resources.rs
@@ -0,0 +1,86 @@
+use dioxus::prelude::*;
+
+#[derive(serde::Deserialize)]
+struct ResourceListApi {
+    header: String,
+    content: String,
+}
+
+const RESOURCELIST_DIR: Asset = asset!("/assets/static/resources/");
+
+#[component]
+pub fn ResourceLists() -> Element {
+    // let path = RESOURCELIST_DIR.resolve();
+    // let Content: Element = match std::fs::read_dir(path) {
+    //     Ok(x) => rsx! {
+    //         "a"
+    //     },
+    //     Err(_) => rsx! {
+    //         "b"
+    //     },
+    // }; 
+
+    rsx! {
+        h2 {"Resources"}
+        p { "These are unorganized lists of resources on various topics that I decided to collect at some point in the beginning of my PhD" }
+        // {Content}
+        ul {
+            li {
+                // a { href: "https://math.columbia.edu/~magenroy/resources.html", "Unsorted resources" }
+                a { href: "Columbia/resources.html", "Unsorted resources" }
+            }
+            li {
+                // a { href: "https://math.columbia.edu/~magenroy/DAG-resources.html", "DAG/SAG resources" }
+                a { href: "Columbia/DAG-resources.html", "DAG/SAG resources" }
+            }
+            li {
+                // a { href: "https://math.columbia.edu/~magenroy/Motives-resources.html", "Motives resources" }
+                a { href: "Columbia/Motives-resources.html", "Motives resources" }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn ResourceList(name: String) -> Element {
+    // let path = format!("assets/static/resources/{}.toml", name);
+
+    let mut path = RESOURCELIST_DIR.resolve();
+    path.push(&name);
+
+
+    let Content: Element = match std::fs::read_to_string(path) {
+        Ok(c) => {
+            match toml::from_str(&c) {
+                Ok(ResourceListApi { header, content }) => {
+                    rsx!{
+                        header { div { class: "content", "{header}" } }
+                        main { class: "content",
+                            dangerous_inner_html: "{content}",
+                        }
+                    }
+                },
+                Err(_) => rsx! {
+                        main { class: "error",
+                        h1 { "Resource list error" }
+                        p { "Error parsing resource list" }
+                        pre { color: "red", "log:\nattemped to load {name:?}" }
+                    }
+                }
+            }
+        },
+        Err(_) => rsx! {
+            main { class: "error",
+                h1 { "Resource list not found" }
+                p { "The page you requested doesn't exist." }
+                pre { color: "red", "log:\nattemped to load {name:?}" }
+            }
+        }
+    };
+
+    rsx! {
+        main {
+            {Content}
+        }
+    }
+}
---
+++ src/components/seminars.rs
@@ -0,0 +1,89 @@
+use dioxus::{logger, prelude::*};
+
+#[derive(serde::Deserialize)]
+struct SeminarApi {
+    header: String,
+    main: String,
+}
+
+// const SEMINAR_STYLE: Asset = asset!("/assets/styling/seminar.css");
+
+const SEMINAR_DIR: Asset = asset!("/assets/static/seminars/");
+
+#[component]
+pub fn Seminars() -> Element {
+    // let path = SEMINAR_DIR.resolve();
+    // let Content: Element = match std::fs::read_dir(path) {
+    //     Ok(x) => rsx! {
+    //         "a"
+    //     },
+    //     Err(_) => rsx! {
+    //         "b"
+    //     },
+    // }; 
+
+    rsx! {
+            h2 {"Seminars"}
+            p { "Seminars I have organized:" }
+            // {Content}
+            ul {
+                li {
+                    // a { href: "https://math.columbia.edu/~magenroy/DAG-seminar.html", "Derived algebraic geometry seminar at Columbia University" }
+                    a { href: "Columbia/DAG-seminar.html", "Derived algebraic geometry seminar at Columbia University" }
+                }
+                li {
+                    // a { href: "https://math.columbia.edu/~magenroy/motivicseminar.html", "Motivic homotopy theory seminar at Columbia University" }
+                    a { href: "Columbia/motivicseminar.html", "Motivic homotopy theory seminar at Columbia University" }
+                }
+                li {
+                    // a { href: "https://math.columbia.edu/~magenroy/MilnorWittMotivesSeminar.html", "Milnor-Witt motives seminar at Columbia University" }
+                    a { href: "Columbia/MilnorWittMotivesSeminar.html", "Milnor-Witt motives seminar at Columbia University" }
+                }
+            }
+        }
+}
+
+#[component]
+pub fn Seminar(name: String) -> Element {
+    // let path = format!("assets/static/seminars/{}.toml", name);
+
+    let mut path = SEMINAR_DIR.resolve();
+    path.push(&name);
+
+    logger::tracing::debug!("{:?}", path);
+
+    let Content: Element = match std::fs::read_to_string(path) {
+        Ok(c) => {
+            match toml::from_str(&c) {
+                Ok(SeminarApi { header, main }) => rsx! {
+                    header { div { class: "content",
+                        dangerous_inner_html: "{header}",
+                    } }
+                    main { class: "content", class: "seminar",
+                        dangerous_inner_html: "{main}",
+                    }
+                },
+                Err(_) => rsx! {
+                    main { class: "error",
+                        h1 { "Error loading seminar webpage" }
+                        pre { color: "red", "log:\nattemped to load {name:?}" }
+                    }
+                }
+            }
+        },
+        Err(_) => rsx! {
+            main { class: "error",
+                h1 { "Seminar not found" }
+                p { "The page you requested doesn't exist." }
+                pre { color: "red", "log:\nattemped to load {name:?}" }
+            }
+        }
+    };
+
+    rsx! {
+        // document::Link { rel: "stylesheet", href: SEMINAR_STYLE }
+        main {
+            {Content}
+        }
+    }
+}
